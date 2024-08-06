a4314c2b826b781e118e535edb7cf1bbbbd1a14f
tanisha23rout <hitanisharout@gmail.com>
2024-08-07 00:04:58 +0530

    First commit

---
+++ src/middlewares/fileupload.middleware.js
@@ -0,0 +1,21 @@
+// 1. Import multer.
+import multer from 'multer';
+import path from 'path';
+
+// 2. Configure storage with filename and location.
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
+
+export const upload = multer({
+  storage: storage,
+});
